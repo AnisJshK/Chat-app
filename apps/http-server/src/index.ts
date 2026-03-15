2e8541d798201c2d27d16d97a25f03cd2b6165f8
AnisJshK <anisshaikh2131@gmail.com>
2026-03-15 16:57:02 +0530

    fixed client

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,11 +1,38 @@
 import express from "express"
 import cors from 'cors';
+import {CreateUserSchema} from "@repo/common/types"
+import {prismaClient} from "@repo/db/client"
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/signup",(req,res)=>{
+app.post("/signup",async(req,res)=>{
+    const parsed = CreateUserSchema.safeParse(req.body);
+    if(!parsed.success){
+        res.json({
+            message:"Incorrect Inputs!!!!"
+        })
+        return;
+    }
+    
+    try {
+        const user = await prismaClient.user.create({
+            data:{
+                email:parsed.data?.email,
+                password:parsed.data?.password,
+            }
+        })
+        res.json({
+            userId : user.id
+        })
+        
+    } catch (error) {
+        res.status(411).json({
+            message:"User already exists with this username"
+        })
+    }
+
 
 })
 app.post("/signin",(req,res)=>{
