6b7484740dc192762cbe59dd9532745e68e78d88
AnisJshK <anisshaikh2131@gmail.com>
2026-03-15 16:49:27 +0530

    second commit

--- packages/db/prisma/migrations/20260315103716_init_schema/migration.sql
+++ packages/db/prisma/migrations/20260315103716_init_schema/migration.sql
@@ -1,7 +1,7 @@
 -- CreateTable
 CREATE TABLE "User" (
     "id" TEXT NOT NULL,
-    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
@@ -39,7 +39,7 @@ CREATE TABLE "Message" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "RoomMember_userId_roomId_key" ON "RoomMember"("userId", "roomId");
