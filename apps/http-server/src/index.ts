bf11b11cdf2269f156ab4f0df70918c7a913fda1
AnisJshK <anisshaikh2131@gmail.com>
2026-03-16 23:07:30 +0530

    Http-completed

--- apps/http-server/src/index.ts
+++ apps/http-server/src/index.ts
@@ -1,13 +1,19 @@
 import express from "express"
 import cors from 'cors';
-import {CreateUserSchema} from "@repo/common/types"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+// import {JWT_SECRET} from '@repo/backend-common';
+import {CreateUserSchema, RoomSchema} from "@repo/common/types"
 import {prismaClient} from "@repo/db/client"
+import userMiddleware from "./userMiddleware";
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.post("/signup",async(req,res)=>{
+const JWT_SECRET = "34gfgarg45gfbrw";
+
+app.post("/user/signup",async(req,res)=>{
     const parsed = CreateUserSchema.safeParse(req.body);
     if(!parsed.success){
         res.json({
@@ -15,13 +21,23 @@ app.post("/signup",async(req,res)=>{
         })
         return;
     }
-    
+
+    const {email,password} = parsed.data;
+
+    const userexists = await prismaClient.user.findUnique({where:{email}})
+    if(userexists){
+        res.status(402).json({
+            message:"User already exists!!!!"
+        })
+    }
+   
     try {
+         const hashedPassword = await bcrypt.hash(password,10);
         const user = await prismaClient.user.create({
             data:{
                 email:parsed.data?.email,
-                password:parsed.data?.password,
-            }
+                password:hashedPassword,
+            },
         })
         res.json({
             userId : user.id
@@ -35,16 +51,130 @@ app.post("/signup",async(req,res)=>{
 
 
 })
-app.post("/signin",(req,res)=>{
+app.post("/user/signin",async(req,res)=>{
+    try {
+        const {email,password} = req.body;
+
+        const user = await prismaClient.user.findUnique({where:{email}});
+        if(!user){
+            return res.status(400).json({
+                message:"User not found!!!!"
+            });
+        }
+
+        const valid = await bcrypt.compare(password,user.password);
+        if(!valid){
+            return res.status(400).json({
+                message:"Password Incorrect!!!"
+            })
+        }
+
+        const token = jwt.sign({
+            userId : user.id,
+        },JWT_SECRET);
+
+        res.json({
+            token
+        })
+    } catch (error) {
+        res.json({
+            message:error
+        })
+    }
+})
+app.post("/user/createroom",userMiddleware,async(req:any,res)=>{
+     const parsed = RoomSchema.safeParse(req.body);
+     if(!parsed.success){
+        res.status(400).json({
+            message:parsed.error.message,
+        })
+     }
+     const {name} = parsed.data!;
+     try {
+        const room = await prismaClient.room.create({
+            data:{
+                name
+            }
+        })
+        await prismaClient.roomMember.create({
+            data:{
+                roomId:room.id,
+                userId:req.userId
+            }
+        })
+        res.json({
+            roomId:room.id
+        });
+     } catch (error) {
+        //  console.log(error);
+        return res.status(500).json({
+            
+            message:"Could not create room",
+            error
+        })
+       
+     }
+})
+app.post("/user/joinroom",userMiddleware,async(req:any,res)=>{
+    const {roomId} = req.body;
+
+    try {
+        await prismaClient.roomMember.create({
+            data:{
+                roomId,
+                userId:req.userId
+            }
+        });
+        res.json({
+            message:"Joined room successfully!!!"
+        })
+    } catch (error) {
+        console.log(error)
+
+        // return res.status(400).json({
+        //     message:"Already Joined room or invalid room!!!"
+        // })
+    }
 
 })
-app.post("/createroom",(req,res)=>{
 
+app.get("/user/rooms",userMiddleware,async(req:any,res)=>{
+    const rooms = await prismaClient.roomMember.findMany({
+        where:{
+            userId:req.userId
+        },
+        include:{
+            room:true
+        }
+    });
+    res.json({
+        rooms
+    })
 })
-app.get("/room",(req,res)=>{
 
+app.get("/messages/:roomId",userMiddleware,async(req:any,res)=>{
+    const roomId = req.params.body;
+
+    const messages = await prismaClient.message.findMany({
+        where:{
+            roomId
+        },
+        include:{
+            user:{
+                select:{
+                    email:true
+                }
+            }
+        },
+        orderBy:{
+            createdAt:"desc"
+        },
+        take:50
+    })
+    res.json({
+        messages
+    })
 })
-
 
 const PORT = 3001;
 
---
+++ apps/http-server/src/userMiddleware.ts
@@ -0,0 +1,48 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from 'jsonwebtoken'
+
+const JWT_SECRET = "34gfgarg45gfbrw";
+export interface JwtPayload {
+  userId: string;
+}
+
+declare global {
+  namespace Express {
+    interface Request {
+      userId?: string;
+    }
+  }
+}
+export default function userMiddleware(req:Request,res:Response,next:NextFunction){
+
+    const header = req.headers.authorization;
+    if(!header||!header.startsWith("Bearer")){
+        return res.status(401).json({
+            error:"Missing or malformed token"
+        })
+        
+    }
+    const token = header.split(" ")[1];
+    if(!token){
+        return res.status(401).json({
+            error:"Missing token"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token,JWT_SECRET as string) as unknown as JwtPayload;
+        req.userId = decoded.userId;
+        next();
+        
+    } catch (error) {
+        if(error instanceof jwt.TokenExpiredError){
+            return res.status(401).json({error:"Token expired!!"});
+        }
+        return res.status(401).json({
+            error:"Invalid Token!!!"
+        })
+        
+    }
+
+
+}
